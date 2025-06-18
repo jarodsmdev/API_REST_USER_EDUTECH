b682eaf5f25e7625cc0e226ee4648de5ebf1076a
Jarod Smith <lbriones.dev@gmail.com>
2025-06-18 00:21:29 -0400

    Rem/Quita importaciones no utilizadas

--- src/main/java/com/briones/users/management/service/UserServiceImpl.java
+++ src/main/java/com/briones/users/management/service/UserServiceImpl.java
@@ -9,7 +9,6 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
