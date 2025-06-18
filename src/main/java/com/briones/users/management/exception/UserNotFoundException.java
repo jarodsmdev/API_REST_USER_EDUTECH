b83cd0ea3841e266eb4294aa9e753503a8ca8f2c
Jarod Smith <lbriones.dev@gmail.com>
2025-06-18 14:56:14 -0400

    chorus/Refactorización de carpetas

--- src/main/java/com/briones/users/management/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.briones.users.management.exception;
-
-public class UserNotFoundException extends Exception{
-    public UserNotFoundException(String message){
-        super(message);
-    }
-}
